39c6ef1f2bd2a8257c9b253b9648e6afc0b2992b
Anthony Guzman <113675572+AnthonyGzm@users.noreply.github.com>
2024-04-19 11:29:40 -0400

    Add Users Methods & Interfaces

---
+++ READvolution/Services/ServicioUsuario.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using READvolution.Models;
+using READvolution.Models.Entidades;
+
+namespace READvolution.Services
+{
+    public class ServicioUsuario : IServicioUsuario
+    {
+        private readonly READvolutionContext _context;
+        
+        public ServicioUsuario(READvolutionContext context)
+        {
+            _context = context;
+        }
+        public async Task<Usuario> GetUsuario(string correo, string clave)
+        {
+            Usuario usuario = await _context.Usuarios.Where(u => u.Correo == correo && u.Clave == clave)
+                .FirstOrDefaultAsync();
+            return usuario;
+        }
+
+
+        public async Task<Usuario> GetUsuario(string nombreUsuario)
+        {
+            return await _context.Usuarios
+                .FirstOrDefaultAsync(u => u.NombreUsuario == nombreUsuario);
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+    }
+}
