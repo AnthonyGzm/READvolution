e10f91a218679b6b468f0f8c547c25975c2731fa
Anthony Guzman <113675572+AnthonyGzm@users.noreply.github.com>
2024-04-19 11:57:20 -0400

    Added list services for authors and other related functionalities.

---
+++ READvolution/Services/ServicioLista.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using READvolution.Models;
+
+namespace READvolution.Services
+{
+    public class ServicioLista : lServicioLista
+    {
+
+        private readonly READvolutionContext _context;
+        public async Task<IEnumerable<SelectListItem>> GetListaAutores()
+        {
+            List<SelectListItem> list = await _context.Autores.Select(x => new SelectListItem
+            {
+                Text = x.Nombre,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione Un Autor...]",
+                Value = "0"
+            });
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetListaCategoria()
+        {
+            List<SelectListItem> list = await _context.Categorias.Select(x => new SelectListItem
+            {
+                Text = x.Nombre,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione Una Categoria...]",
+                Value = "0"
+            });
+            return list;
+        }
+    }
+}
---
+++ READvolution/Services/lServicioLista.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace READvolution.Services
+{
+    public interface lServicioLista
+    {
+        Task<IEnumerable<SelectListItem>> GetListaAutores();
+        Task<IEnumerable<SelectListItem>> GetListaCategoria();
+
+    }
+}
