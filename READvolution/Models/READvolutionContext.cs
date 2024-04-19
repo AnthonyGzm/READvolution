9c425ac4fafc5765eb5399d13bfa59fb80656b71
Anthony Guzman <113675572+AnthonyGzm@users.noreply.github.com>
2024-04-19 10:04:07 -0400

    Context is created

---
+++ READvolution/Models/READvolutionContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using READvolution.Models.Entidades;
+
+namespace READvolution.Models
+{
+    public class READvolutionContext : DbContext
+    {
+        public READvolutionContext(DbContextOptions<READvolutionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Categoria>().HasIndex(c => c.Nombre).IsUnique();
+        }
+
+    }
+}
